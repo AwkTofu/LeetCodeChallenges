7cc7d7ae24c3462732db57cce4c1a70a2e3b87d9
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-07-23 19:22:25 -0400

    some logic went wrong, restarting

--- add_two_numbers.js
+++ add_two_numbers.js
@@ -22,6 +22,31 @@ Explanation: 342 + 465 = 807.
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    console.log(l1)
+     console.log(l1)
     console.log(l2)
+    
+    var test = new ListNode(); 
+    test.val(4);
+    console.log("test", test)
+    
+    
+    let i = 0;
+    let result = l1;
+    let carryOver = 0;
+    console.log("len", l2.val, l2.next, l2.next)
+    l2.data
+    while (i < l2.length)
+    {
+        let num = result[i] + l2[i];
+        if (num >= 10)
+        {
+            num -= 10;
+            carryOver = 1;
+        }
+        console.log("num:", num, "carryOver:", carryOver);
+        result[i] = num + carryOver;
+        carryOver = 0;
+    }
+    
+    return result;
 };
