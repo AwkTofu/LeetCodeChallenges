161fe4deaca364d3ced17e9334cfaccd330bf59e
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-07-23 18:26:12 -0400

    edited out the console.logs

--- two_sums.js
+++ two_sums.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    console.log("test", nums, target);
+    //console.log("test", nums, target);
     
     if (nums.length < 2)
         console.log("This will break.")
@@ -12,17 +12,17 @@ var twoSum = function(nums, target) {
     //No need for i to reach last number since J will reach it.
     for (let i = 0; i<nums.length - 1; i++)
     {
-        console.log(nums[i]);
+        //console.log(nums[i]);
         for (let j = i + 1; j<nums.length; j++)
         {
-            console.log("J is ", nums[j]);
-            console.log("Target: ",target,"I + J", nums[i] + nums[j] ,nums[i] + nums[j] === target)
+            //console.log("J is ", nums[j]);
+            //console.log("Target: ",target,"I + J", nums[i] + nums[j] ,nums[i] + nums[j] === target)
             if (nums[i] + nums[j]=== target)
             {
                 let result = [i, j]
-                console.log("Result" , result);
+                //console.log("Result" , result);
                 return [i, j];
-            }
+            
         }
     }
 };
