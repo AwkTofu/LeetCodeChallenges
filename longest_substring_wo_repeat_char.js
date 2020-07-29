543db5283c5571f9bd539b069935dc354abe1c50
FanZhong Zeng <fanzhongzeng78@gmail.com>
2020-07-29 00:41:32 -0400

    FinalSolutionAccepted

--- longest_substring_wo_repeat_char.js
+++ longest_substring_wo_repeat_char.js
@@ -23,7 +23,7 @@ Explanation: The answer is "wke", with the length of 3.
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let longestLength = 0;
+    /*let longestLength = 0;
     //var s = "aab"
     let currentWord = "";
     let currentLength = 0;
@@ -54,5 +54,47 @@ var lengthOfLongestSubstring = function(s) {
     console.log("Word:", s, "currentLen",currentLength, "longestLen:", longestLength)
     if (currentLength > longestLength)
         return currentLength;
-    return longestLength;
+    return longestLength;*/
+
+    //****************Restart Got Stuck, this is different approach***********************
+    let longestLen = 0;
+    let curr = 0;
+    //console.log(s[1])
+    
+    //var s = "anviaj"
+    
+    for (let i = 0; i < s.length; i++)
+    {
+        if (longestLen > s.length - i)
+        {
+            break;
+        }
+        
+        let currentWord = s[i];
+        let currentLen = 1;
+        for (let j = i + 1; j< s.length; j++)
+        {
+            if (currentWord.includes(s[j]))
+            {
+                if (currentLen > longestLen)
+                {
+                    longestLen = currentLen;
+                }
+                break;
+            }
+            else
+            {
+                currentWord += s[j];
+                currentLen++;
+            }
+        }
+        if (currentLen > longestLen)
+        {
+            longestLen = currentLen;
+        }
+        
+        //console.log("CurrentLetter", currentWord, "currentLen", currentLen, "LongestLen", longestLen)
+    }
+    //console.log("Word:", s, "longestLen:", longestLen)
+    return longestLen;
 };
